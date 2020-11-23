05fa45f2f4aa8362afb36682597ef9ec9afdfacf
JakubChecinski <40202132+JakubChecinski@users.noreply.github.com>
2020-11-23 17:33:32 +0100

    Reset items before new iteration

--- Engine/Index.cs
+++ Engine/Index.cs
@@ -6,11 +6,7 @@ using Game.Engine.Items;
 using Game.Engine.Items.ItemFactories;
 using Game.Engine.Items.BasicArmor;
 using Game.Engine.Interactions.InteractionFactories;
-using Game.Engine.Items.BasicRing;
-using Game.Engine.Items.Amulet;
-using Game.Engine.Items.OsirisArmor;
-using Game.Engine.Items.AmuletsAndPotions;
-using Game.Engine.Items.Shield;
+
 
 namespace Game.Engine
 {
@@ -37,65 +33,13 @@ namespace Game.Engine
             new AntiMagicArmor(),
             new BerserkerArmor(),
             new GrowingStoneArmor(),
-            new CrystalSword(),
-            new InfinitySword(),
-            new MoonlightSword(),
-            new DemonSword(),
-            new GreatSword(),
-            new ButchersSword(),
-            new Katana(),
-            new ArmorRing(),
-            new HealthRing(),
-            new InfinityRings(),
-            new MageRing(),
-            new LuckyCharmAmulet(),
-            new MageDuelistAmulet(),
-            new SupportingAmulet(),
-            new AntiPoisonAmulet(),
-            new AmuletOfHealing(),
-            new AmuletOfNegotiation(),
-            new FireproofAmulet(),
-            new OsirisEye(),
-            new OsirisSabre(),
-            new OsirisStaff(),
-            new IronAxe(),
-            new GymirAxe(),
-            new DiamondAxe(),
-            new FlameStaff(),
-            new SafetyRod(),
-            new WindStaff(),
-            new AirStone(),
-            new WaterStone(),
-            new EarthStone(),
-            new FireStone(),
-            new GoldStone(),
-            new AntiMagicHealthStone(),
-            new GoldenShield(),
-            new MirrorShield(),
-            new TurtleShield(),
-            new Narya(),
-            new Vilya(),
-            new Nenya(),
-            new MagiciansCan(),
-            new MagnificentCape(),
-            new SingleUseArmor(),
-            new StylisedFlatIron(),
             // quest items (if applicable) start below
+            new GymirAxe(),
         };
 
         private static List<ItemFactory> itemFactories = new List<ItemFactory>()
         {
             new BasicArmorFactory(),
-            new AxeFactory(),
-            new SwordFactory(),
-            new StaffFactory(),
-            new BasicRingFactory(),
-            new AmuletFactory(),
-            new StonesFactory(),
-            new OsirisFactory(),  
-            new ShieldFactory(),
-            new ElvenRingFactory(),
-            new MildlyFunnyItemFactory(),
         };
 
         private static List<MonsterFactory> monsterFactories = new List<MonsterFactory>()
