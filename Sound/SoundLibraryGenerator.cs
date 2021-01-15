137f2217358d1048bd11cfcf6b91472bc07a4acb
matskywacker <mactotak@gmail.com>
2021-01-15 21:17:03 +0100

    Add Sound Engine and Library

---
+++ Sound/SoundLibraryGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Sound
+{
+    public static class SoundNames
+    {
+        public const string BACKGROUND_MUSIC = "BackgroundMusic";
+        
+
+    }
+    internal static class SoundLibraryGenerator
+    {
+        internal static List<Sound> CreateLibrary()
+        {
+            List<Sound> soundLibrary = new List<Sound>();
+            string monster = string.Empty;
+            string item = string.Empty;
+
+
+            return soundLibrary;
+        }
+    }
+}
