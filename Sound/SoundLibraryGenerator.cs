c0cb80fe0f8c3c86cb9b97aa9d01ca8f0cc7c394
matskywacker <mactotak@gmail.com>
2021-01-15 21:53:50 +0100

    Add Sound Names constant strings

--- Sound/SoundLibraryGenerator.cs
+++ Sound/SoundLibraryGenerator.cs
@@ -9,7 +9,18 @@ namespace Game.Sound
     public static class SoundNames
     {
         public const string BACKGROUND_MUSIC = "BackgroundMusic";
-        
+        public const string BACKGROUND_MUSIC_MENU = "BackgroundMusicMenu";
+        public const string BACKGROUND_MUSIC_GAME = "BackgroundMusicGame";
+        public const string BACKGROUND_MUSIC_BATTLE = "BackgroundMusicBattle";
+
+        public const string MOUSE_HOVER_MENU = "MouseHoverMenu";
+        public const string MOUSE_CLICK_MENU = "MouseClickMenu";
+
+        public const string MOUSE_HOVER_GAME = "MouseHoverGame";
+        public const string MOUSE_CLICK_GAME_1 = "MouseClickGame_1";
+        public const string MOUSE_CLICK_GAME_2 = "MouseClickGame_2";
+
+        public const string PLAYER_DEATH = "PlayerDeath";
 
     }
     internal static class SoundLibraryGenerator
