1ac1ff44929dd3561e78669d1e70f579f7039310
MatSky <mactotak@gmail.com>
2021-01-28 03:23:06 +0100

    SoundEngine documentation
    Added comments to classes and methods

--- Sound/SoundEngine.cs
+++ Sound/SoundEngine.cs
@@ -8,6 +8,10 @@ using Game.Sound.Players;
 
 namespace Game.Sound
 {
+    /// <summary>
+    /// Handles sound implementation
+    /// Contains two players: one for background music and one for effects
+    /// </summary>
     public class SoundEngine
     {
         private BasicSoundPlayer backgroundPlayer;
@@ -21,53 +25,93 @@ namespace Game.Sound
             this.soundPlayer = new BasicSoundPlayer();
         }
 
+        /// <summary>
+        /// Plays background music for specified location in game navigation 
+        /// by current game sound context (check SoundEngine.GetBackgroundMusic())
+        /// </summary>
         public void PlayBackgroundMusic()
         {
             backgroundPlayer.Open(GetBackgroundMusic());
             backgroundPlayer.PlayLooped();
         }
 
+        /// <summary>
+        /// Stops background music player gently.
+        /// Use it while entering another game music context eg. Battle, GamePage, MenuPage.
+        /// </summary>
         public void StopBackgroundMusic()
         {
             backgroundPlayer.Stop();
         }
 
+        /// <summary>
+        /// Force stops background music player. 
+        /// Use for eg. while ending app etc.
+        /// </summary>
         public void ForceStopBackgroundMusic()
         {
             backgroundPlayer.ForceStop();
         }
 
+        /// <summary>
+        /// Pause background music
+        /// Use it while entering new context with probability of comming back to previous. 
+        /// </summary>
         public void PauseBackgroundMusic()
         {
             backgroundPlayer.Pause();
         }
 
+        /// <summary>
+        /// Resume paused background music. 
+        /// </summary>
         public void ResumeBackgroundMusic()
         {
             backgroundPlayer.Play();
         }
 
+        /// <summary>
+        /// Getting background music Sound obj. based on current game context. 
+        /// </summary>
+        /// <returns>Sound type music background object.</returns>
         private Sound GetBackgroundMusic()
         {
             return SoundLibrary.Sounds.Find(t => t.SoundType == SoundType.Background && t.SoundContext == soundContext);
         }
 
+        /// <summary>
+        /// Plays sound by its name. 
+        /// </summary>
+        /// <param name="soundName">Name of sound to play</param>
         public void PlaySound(string soundName)
         {
             PlaySound(SoundLibrary.Sounds.Find(t => t.SoundName == soundName));
         }
 
+        /// <summary>
+        /// Plays sound by its name and sound type. 
+        /// </summary>
+        /// <param name="soundName">Name of sound to play. Must not be null, otherwise doesn't play anything.</param>
+        /// <param name="soundType">Type of sound. Must not be null, otherwise doesn't play anything.</param>
         public void PlaySound(string soundName, SoundType soundType)
         {
             PlaySound(SoundLibrary.Sounds.Find(t => t.SoundName == soundName && t.SoundType == soundType));
         }
 
+        /// <summary>
+        /// Plays sound using embedded self-managed playlist. 
+        /// </summary>
+        /// <param name="soundName">Name of sound to play. Must not be null, otherwise doesn't play anything.</param>
+        /// <param name="soundType">Type of sound. Must not be null, otherwise doesn't play anything.</param>
         public void WaitAndPlay(string soundName, SoundType soundType)
         {
             soundPlayer.WaitAndPlay(SoundLibrary.Sounds.Find(t => t.SoundName == soundName && t.SoundType == soundType));
         }
 
-
+        /// <summary>
+        /// Play given sound.
+        /// </summary>
+        /// <param name="s">Sound type object.</param>
         private void PlaySound(Sound s)
         {
             if (s != null)
@@ -77,12 +121,18 @@ namespace Game.Sound
             }
         }
 
+        /// <summary>
+        /// Stops all actively running sound players. 
+        /// </summary>
         public void StopAllPlayers()
         {
             backgroundPlayer.Stop();
             soundPlayer.Stop();
         }
 
+        /// <summary>
+        /// Force stops all actively running sound players.
+        /// </summary>
         public void ForceStopAllPlayers()
         {
             backgroundPlayer.ForceStop();
